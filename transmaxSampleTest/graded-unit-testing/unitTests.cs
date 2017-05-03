6c2f1533ef2d28524398ed03510b96d8b8ba5574
aaron <aaron.newton@connect.qut.edu.au>
2017-05-03 15:58:54 +1000

    first git commit

---
+++ transmaxSampleTest/graded-unit-testing/unitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using grade_scores;
+
+namespace graded_unit_testing {
+    [TestClass]
+    public class unitTests {
+        private void helperTestMethod1() {
+
+        }
+
+        [TestMethod, TestCategory("grade_scores")]
+        public void TestMethod1() {
+            string[] newArgs = new string[1];
+            grade_scores.Program myProgram = new grade_scores.Program();
+            string output = myProgram.getOutputLocation("classA-grades.txt");
+            Assert.AreEqual("classA-grades-graded.txt", output);
+            //Ok what should I test.
+            //There are like 6 methods. The true main, and writeOut, sortArray, stringArrayToLineInfoArray, getLastFileName, getOutputLocation, also LineInfo
+
+            //grade_scores.;
+            //Assert.AreEqual();
+        }
+
+        public void TestMethod2() {
+            string[] newArgs = new string[1];
+            grade_scores.Program myProgram = new grade_scores.Program();
+            string output = myProgram.getOutputLocation("classA-grades.txt");
+            Assert.AreEqual("", output);
+            //There are like 6 methods. The true main, and writeOut, sortArray, stringArrayToLineInfoArray, getLastFileName, getOutputLocation, also LineInfo
+        }
+    }
+}
