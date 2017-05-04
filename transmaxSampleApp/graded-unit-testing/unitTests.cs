14c0b15b8f0ad738d1fb6d04bb30b9ab94fcaa82
aaron <aaron.newton@connect.qut.edu.au>
2017-05-04 12:49:06 +1000

    makes proper directory path, seems consistant, check if works in app

--- transmaxSampleApp/graded-unit-testing/unitTests.cs
+++ transmaxSampleApp/graded-unit-testing/unitTests.cs
@@ -16,6 +16,13 @@ namespace graded_unit_testing {
             file.Close();
         }
 
+
+        private String usingDirectory() {//Method to easily standardise the directory used for files made 
+            String path = "C:\\sampleGradeFolder";
+            System.IO.Directory.CreateDirectory(path);
+            return path;
+        }
+
         [TestMethod, TestCategory("grade_scores_methods")]
 
         public void getValidOutputFileLocation() {
@@ -31,15 +38,14 @@ namespace graded_unit_testing {
             string[] newArgs = new string[1];
             string[] fileLines = { "Smith, John, 32", "Spencer, Joel, 85", "Spencer, Cody, 85" };
 
-            newArgs[0] = "C:\\grade.txt";
+            newArgs[0] = usingDirectory()+"\\grade.txt";
             //newArgs[0] = "C:\\Sector\\General Content\\Git_Folder\\transmaxRepo\\sampleOutput\\grade2.txt";
             writeSampleFile(newArgs[0], ref fileLines);
 
             //Determine created text files contents
             grade_scores.grade_scores myProgram = new grade_scores.grade_scores();
             myProgram.mainGradeScoresMethod(newArgs);
-            //string[] wrote = System.IO.File.ReadAllLines("C:\\Sector\\General Content\\Git_Folder\\transmaxRepo\\sampleOutput\\grade2-graded.txt");
-            string[] wrote = System.IO.File.ReadAllLines( "C:\\grade2-graded.txt");
+            string[] wrote = System.IO.File.ReadAllLines(usingDirectory()+"\\grade-graded.txt");
             //Console.WriteLine("");
             Assert.AreEqual(fileLines[2], wrote[0]);
             Assert.AreEqual(fileLines[1], wrote[1]);
